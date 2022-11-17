adfc1b199b7e0441ed23cec66726bff0fdc196e6
Geovanna Lessa <geovannalessa2@gmail.com>
2022-11-16 21:59:39 -0300

    Criado a rota e controllers para as novas páginas criadas de entrar e cadastrar

--- controller/homeController.js
+++ controller/homeController.js
@@ -2,8 +2,15 @@ const homeController = {
     mostraHome:(req,res)=>{
         res.render('home.ejs')
     },
+    //telas de login
     mostraApelido:(req, res)=>{
         res.render("login.ejs")
+    },
+    mostraCadastro:(req,res)=>{
+        res.render("cadastro.ejs")
+    },
+    mostraEntrar:(req,res)=>{
+        res.render("entrar.ejs")
     }
 }
 
--- router/homerRouter.js
+++ router/homerRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get('/', homeController.mostraHome);
 router.get('/login', homeController.mostraApelido);
+router.get('/cadastro', homeController.mostraCadastro);
+router.get('/entrar', homeController.mostraEntrar);
 
 module.exports = router;
